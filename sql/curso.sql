c2688a86ed4d2d2cada86be1e2d08d9e6a9ce6e5
J. Daniel Salcedo Sambrano <dsalcedo@outlook.com>
2012-12-13 15:18:57 -0600

    se crea alumnos
    se crea tabla de nombre alumnos

--- sql/curso.sql
+++ sql/curso.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE IF NOT EXISTS `curso` 
+CREATE DATABASE IF NOT EXISTS `curso`; 
+USE `curso`;
+
+CREATE TABLE `alumnos` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(150) NULL DEFAULT NULL,
+  `email` VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+COMMENT='contiene la informacion de alumnos'
+COLLATE='latin1_swedish_ci'
+ENGINE=MyISAM;
